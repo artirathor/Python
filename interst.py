17a7d1bba5cac181271ba16fb4ad95f96fe5a917
artirathor <artirathor2022@gmail.com>
2026-02-24 07:29:52 +0530

    add simple interest

---
+++ interst.py
@@ -0,0 +1,11 @@
+p = 1000
+r = 5
+t = 3
+si = (p*r*t)/100
+print(si)
+
+def simple_intst(p,r,t):
+    si = p*r*t/100
+    return si
+interest=(simple_intst(5000,5,2))   
+print("interest=",interest) 
