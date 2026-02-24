d56b25a443ed2e2ebbcaac446307d768e69e5db6
artirathor <artirathor2022@gmail.com>
2026-02-24 07:15:43 +0530

    addition

---
+++ add.py
@@ -0,0 +1,34 @@
+a,b, = 3,5
+c = a + b
+print(c)
+
+a = 3 
+b = 6
+add = a + b
+print(add)
+
+def add(a,b):
+    add = a + b
+    return add
+res = (add(2,3))  
+print(res)  
+
+a = [1,2,3,4,5]
+total = (sum(a))
+print(total)
+
+b = [1,3,5,7,9]
+addition = 0
+for x in b:
+    addition = addition + x
+    print(x)
+print(addition)
+
+num = [1,3,6,8,2]
+new_num = []
+max1 = (max(num))
+print(max1)
+num.remove(max1)
+print(num)
+max2 = (max(num))
+print(max2)
