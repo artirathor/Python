ab3888404efa591df1eea12a0a0091cf88a0b69b
artirathor <artirathor2022@gamil.com>
2026-03-05 21:53:06 +0530

    program of area of circule

---
+++ Area_circule.py
@@ -0,0 +1,15 @@
+# find area of circule
+radius = 14
+area = (22 /7) * 14*14
+print(area,  "is circle of area")
+
+import math
+r = 14
+area = math.pi * (r ** 2)
+print(area,"is the area of circle")
+
+import math
+r = 5 
+area = math.pi * math.pow(r, 2)
+print(area, "is the area of circule")
+
