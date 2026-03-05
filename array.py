24d75bdb542b9df73ae1876369cec798b24d1dec
Ronak Patel <info@aglowiditsolutions.com>
2026-03-05 11:58:45 +0530

    Add array.py

---
+++ array.py
@@ -0,0 +1,14 @@
+# sum of array
+# arr = [1,4,7,9,12]
+# sum = 0
+# for x in arr:
+#     sum = sum + x
+# print(sum)    
+
+# using sum
+arr = [1,3,5,7,8]
+addition = sum(arr)
+print(addition)
+
+# using reduce method
+
