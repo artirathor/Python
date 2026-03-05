d150246793a7517174418c39cfd677cc53bfe1fb
artirathor <artirathor2022@gamil.com>
2026-03-05 22:09:58 +0530

    maximum number of program

---
+++ maxlist.py
@@ -0,0 +1,13 @@
+num = [40,60,30,10,50]
+maximum = num[0]
+for i in num:
+    if maximum < i:
+        maximum = i
+print(maximum) 
+
+number = [2,6,9,3,10]
+minimum = number[0]
+for value in number:
+    if minimum > value:
+        minimum = value
+print(minimum)  
