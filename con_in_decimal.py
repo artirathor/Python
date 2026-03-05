b0d97bf3a9661a31983ca532d8b2799c014bb5e7
artirathor <artirathor2022@gamil.com>
2026-03-05 21:55:16 +0530

    convert integer into decimal

---
+++ con_in_decimal.py
@@ -0,0 +1,9 @@
+import decimal
+integer = 10
+print(decimal.Decimal(integer))
+print(type(decimal.Decimal(integer)))
+
+from decimal import Decimal
+integer = 100
+print(decimal.Decimal(integer))
+print(type(decimal.Decimal(integer)))
