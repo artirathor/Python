4d92d4ed5e9e18aeeb490860b0f1c7c3f7f481e8
artirathor <artirathor2022@gamil.com>
2026-03-05 21:28:51 +0530

    accending order program

---
+++ accendind.py
@@ -0,0 +1,8 @@
+numbers = [1,5,3,2,8,9,12]
+numbers.sort()
+print("asending numbers :",numbers)
+
+# using sorted method
+numbers = [1,3,6,8,33,55,22,11,87]
+ascending_num = sorted(numbers)
+print(ascending_num)
