2b34a21e450bafd9fe20a794a41d9fbbd20417e2
artirathor <artirathor2022@gamil.com>
2026-03-05 21:06:32 +0530

    find maximum number

---
+++ max.py
@@ -0,0 +1,29 @@
+# using list
+num = [1,5,8,9,44,12]
+maximum = max(num)
+print(maximum)
+
+# using if else condition
+a = 55
+b = 65
+if a > b:
+    print("a is maximum number")
+else:
+    print("b is maximum number")     
+
+# using turnary operatore
+a = 4
+b = 2
+
+maximum = a if a > b else b
+print("Maximum:", maximum)
+
+# using function
+def max(a,b):
+    if a > b :
+        print("40 is maximum number")
+    else :
+        print("13 is greater number")
+max(40,13)
+
+
