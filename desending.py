e2c8599e8cabaa5848c936f44ac4b97609838805
artirathor <artirathor2022@gamil.com>
2026-03-05 22:07:35 +0530

    program of decending order

---
+++ desending.py
@@ -0,0 +1,12 @@
+# numbers = [40, 60, 30, 10, 50]
+
+# numbers.sort(reverse=True) 
+
+# print("Descending order:", numbers)
+
+# using shorted
+num = [1,5,9,1.3,45,4.5]
+
+descending = sorted(num, reverse=True) 
+
+print("Descending order:", descending)
