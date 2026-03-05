172c6602b692eca15e9d675d7532a211a3201c63
artirathor <artirathor2022@gamil.com>
2026-03-05 22:11:50 +0530

    mimimum number of program

---
+++ minimum_list.py
@@ -0,0 +1,11 @@
+number = [2,6,9,3,10]
+minimum = number[0]
+for value in number:
+    if minimum > value:
+        minimum = value
+print(minimum)  
+
+# using sum
+num = [1,4,7,9,11,2,]
+minimum = min(num)
+print(minimum)
