5cbb5ad7a68101e704468450e56cc261c393f412
artirathor <artirathor2022@gamil.com>
2026-03-06 15:35:02 +0530

    checking paliandrom string using slicing

---
+++ palindrom.py
@@ -0,0 +1,16 @@
+string = "madam"
+
+if string == string[::-1]:
+    print("Palindrome String")
+else:
+    print("Not a Palindrome String")
+
+num = "128"
+
+if num == num[::-1]:
+    print("Palindrome String")
+else:
+    print("Not a Palindrome String")
+
+
+
