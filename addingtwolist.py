44799cbc8ea1e2a6999aaee874ecaca39d2df21c
artirathor <artirathor2022@gamil.com>
2026-03-06 10:33:16 +0530

    adding of two list

---
+++ addingtwolist.py
@@ -0,0 +1,23 @@
+list1 = [2,5,7,9]
+list2 = [12,45,67,89]
+list3 = (list1 + list2)
+print(list3)
+
+lst1 = ["a", "v", "b", "d"]
+lst2 = ["a", "r","p"]
+lst3 = []
+lst3.append(lst1 + lst2)
+print(lst3)
+for i in lst3:
+    print(i)
+
+ls1 = [1,3,6,7]
+ls2 = [2,5,8]
+ls3 = [0,5,7]
+ls4 = []
+ls4.extend(ls1)
+ls4.extend(ls2)
+ls4.extend(ls3)
+print(ls4)
+for num in ls4:
+    print("value:", value)     
