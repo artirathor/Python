b9feeeadb23e4fd08c311836ed1138e124ba4696
artirathor <artirathor2022@gamil.com>
2026-03-10 16:40:16 +0530

    remove spaces in string

---
+++ remove_spaces.py
@@ -0,0 +1,15 @@
+stri = "hello welcome to the javascript"
+stri = stri.replace(" ","")
+print(stri)
+
+strin = "wlcome to the python"
+strin1 = strin.split()
+print(strin1)
+result = "".join(strin1)
+print("After join:", result)
+
+college_name = "Dr. R.M.S. foundation"
+new_college_name = college_name.split()
+print(new_college_name)
+final = "".join(new_college_name)
+print(final)
