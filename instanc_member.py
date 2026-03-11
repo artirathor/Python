923b755c9105586e1bd568274ae7ae6f9377e666
artirathor <artirathor2022@gamil.com>
2026-03-11 22:57:41 +0530

    define the instance variable

---
+++ instanc_member.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__ (self,name,age):  
+        self.Name = name
+        self.age = age
+
+    def add(self):    
+        print("hello")
+
+p1 = Person("arti", 29)
+print(p1.Name)
