efebc800fa9530c207af83e966f8b1924c306e4e
artirathor <artirathor2022@gamil.com>
2026-03-11 23:15:45 +0530

    define class object and instance object

---
+++ deffernce.py
@@ -0,0 +1,11 @@
+class Collage:
+    students = 20
+    def __init__ (self,name,subject):
+        self.name = name
+        self.subject = subject
+
+    def detial(self):
+        pass
+
+C1 = Collage("aarav","python")
+print(C1.subject)
