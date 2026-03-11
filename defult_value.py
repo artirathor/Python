f444e7c6ba5d774641ae1dae6154fd63ed9d663c
artirathor <artirathor2022@gamil.com>
2026-03-11 23:32:13 +0530

    explain defult value

---
+++ defult_value.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__ (self,name,age = 23):
+        self.age = age
+        self.name = name
+e1 = Employee("aaru")
+e2 = Employee("aashu",20)
+print(e1.age)
+print(e2.age)
+        
+        
