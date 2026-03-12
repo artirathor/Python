73506bab233f9cd4fc71e7d3c8535adf0ffdedc0
artirathor <artirathor2022@gamil.com>
2026-03-12 15:12:15 +0530

    methods property in class

---
+++ methodsclass.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def add(self,a,b,):
+        return a + b
+    
+    def multi(self,a,b):
+        return(a*b)
+calc = Calculator()
+
+addition = calc.add(6,7) 
+print("addition =", addition)  
+print 
+        
