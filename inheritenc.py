342383b5e5256423b6dd6de80e976a0413e98a77
artirathor <artirathor2022@gamil.com>
2026-03-13 09:42:22 +0530

    create a parent class with child class

---
+++ inheritenc.py
@@ -0,0 +1,19 @@
+class College:
+    def __init__ (self,Name, dicecode):
+        self.name = Name
+        self.dicecode = dicecode
+
+    def information(self):
+        pow
+
+class Student(College):
+    pass
+
+c1 = College("rms foundation of Bhanpura",1212221001)
+print(c1.name)
+
+
+
+
+
+        
