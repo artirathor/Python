8070c78db51e635f87342bd26c998ed27ce2bd90
artirathor <artirathor2022@gamil.com>
2026-03-18 20:21:01 +0530

    Outer loop → number of pyramid

---
+++ nested_loop/pattrng.py
@@ -0,0 +1,9 @@
+for value in range(3):
+    for i in range(1,10):
+        print(" * "*i)    
+    print("-----------")   
+
+for value in range(2):
+    for i in range(1,6):
+        print(" 2 " * i )   
+    print()
