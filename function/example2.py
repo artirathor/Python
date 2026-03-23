facf5b8129ce7666e73bec2202c557ffa1bd1630
artirathor <artirathor2022@gamil.com>
2026-03-23 19:04:06 +0530

    Implement average calculation with args

---
+++ function/example2.py
@@ -0,0 +1,10 @@
+def average(*a):
+    result = sum(a)/len(a)
+    return result
+
+ans1 = average(2,4,5,3,7,8)
+ans2 = average(2,6,8,12,67)
+ans3 = average(3,5,7,9,11,13,15)
+print(ans1)
+print(ans2)
+print(ans3)
