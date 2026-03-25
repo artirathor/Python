df2d4fc6a7637a5017b3c9751f4a6bc76f2ab503
artirathor <artirathor2022@gamil.com>
2026-03-25 16:51:32 +0530

    Check Number of Dimensions

---
+++ numpy_tutorial/check_array.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.array(20)
+arr1 = np.array([2,4,7])
+arr2 = np.array([[2,4],[3,5]])
+arra3 = np.array([[[2,4,5],[3,5,7]],[[2,5,8],[6,7,0]]])
+print(arr.ndim)
+print(arr1.ndim)
+print(arr2.ndim)
+print(arra3.ndim)
