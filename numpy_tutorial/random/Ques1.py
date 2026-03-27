262dc0c7ac59257f4719b26a4bf5a8801c95b5f8
artirathor <artirathor2022@gamil.com>
2026-03-27 15:58:04 +0530

    Examples of random number

---
+++ numpy_tutorial/random/Ques1.py
@@ -0,0 +1,17 @@
+from numpy import random
+
+x = random.randint(100)
+
+print(x)
+
+from numpy import random
+y = random.randint(1,12)
+print(y)
+
+from numpy import random
+z = random.randint(5)
+print(z)
+
+from numpy import random
+arr = random.randint([1,2,3,4,5,6,7])
+print(arr)
