237ab0bb9b5725c8bea002e30f690518fb05b52f
artirathor <artirathor2022@gamil.com>
2026-03-28 15:24:45 +0530

    program of create an array using numpy

---
+++ numpy_tutorial/create_array/Examples.py
@@ -0,0 +1,36 @@
+import numpy as np
+arr = np.array(12)
+print(arr)
+
+import numpy as np
+arr1 = np.array([1,2,3])
+print(arr1)
+
+import numpy as np
+arr2 = np.array([[1,2,3,4]])
+print(arr2)
+
+import numpy as np
+ar3 = np.array([[[1,2,3,3,0]]])
+print(ar3)
+
+import numpy as np
+arr4 = np.array(12),(23)
+print(arr4)
+
+import numpy as np
+arr5 = np.array([[1,2,3,4],[5,6,7,8]])
+print(arr5)
+
+import numpy as np
+arr6 = np.array([[1,2],[3,4]])
+print(arr6)
+
+import numpy as np
+arr7 = np.array([[[1,2,3]],[[4,5,6]]])
+print(arr7)
+
+import numpy as np
+arr8 = np.array([[[12,13],[23,34]],[[1,2],[3,4]]])
+print(arr8)
+
